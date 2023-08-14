a83b1bdf4493d29846150ea1eb0243ba1141bbbf
Steel <yassel.perez.1991@gmail.com>
2023-08-14 19:48:45 -0300

    Put appropied annotation for testing propuses and fancys stuff

--- src/main/java/com/places/PlaceRepository.java
+++ src/main/java/com/places/PlaceRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+// this should be marked as a @repository mal kiff
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     @Query("select p from Place p where distance(p.point, ST_MakePoint(?1, ?2)) <= ?3")
